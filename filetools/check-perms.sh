238d5d00626bea1bb840cfb89ed99ffa3443e12b
Oliver Henriot <Oliver.Henriot@univ-grenoble-alpes.fr>
2020-04-03 10:08:34 +0200

    Permissions checker initial commit

---
+++ filetools/check-perms.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+# Very basic script to set proper permissions and ownerships
+#
+# TODO check rights and security flaws
+# TODO localisation
+# TODO logging
+#
+# Functions
+function param-read () {
+	param=$(grep "^$1" ${CONF} | sed "s/^$1 *= *//")
+	echo ${param}
+}
+function perm-check () {
+	echo "Looking for improper permissions and group ownerships"
+	find . -type f ! -type l ! -perm 660 -exec ls -l {} \;
+	find . -type d ! -perm 770 -exec ls -l {} \;
+	find . ! -type l ! -user $1 -exec ls -l {} \;
+	find . ! -type l ! -group $2 -exec ls -l {} \;
+}
+function perm-mod () {
+	echo "Correcting any errors"
+	find . -type f ! -type l ! -perm 660 -exec chmod 660 {} \;
+	find . -type d ! -perm 770 -exec chmod 770 {} \;
+	find . ! -type l ! -user $[USR} -exec chown $1:$2 {} \;
+	find . ! -type l ! -group $2 -exec chown $1:$2 {} \;
+}
+#
+# Configuration file lookup
+if [ -f /usr/local/etc/check-perms/local.conf ]; then
+	CONF="/usr/local/etc/check-perms/local.conf"
+elif [ -f /usr/local/etc/check-perms/check-perms.conf ]; then
+	CONF="/usr/local/etc/check-perms/check-perms.conf"
+elif [ -f /usr/local/etc/check-perms.conf ]; then
+	CONF="/usr/local/etc/check-perms.conf"
+elif [ -f /etc/check-perms/local.conf ]; then
+	CONF="/etc/check-perms/local.conf"
+elif [ -f /etc/check-perms/check-perms.conf ]; then
+	CONF="/etc/check-perms/check-perms.conf"
+elif [ -f /etc/check-perms.conf ]; then
+	CONF="/etc/check-perms.conf"
+else
+	echo "Configuration file not found, exiting!"
+	echo "Should be in either /etc, /etc/check-perms"
+	echo "/usr/local/etc or /usr/local/etc/check-perms,"
+	echo "either as check-perms.conf in any of these locations"
+	echo "or as local.conf in the check-perms directory"
+	echo "with the last having higher precedence."
+	exit 1
+fi
+#
+# Startup tests
+CUR=$(whoami)
+WKD=$(pwd)
+if [[ $EUID -eq 0 ]]; then
+	echo "This script must not be run as root" 1>&2
+	exit 1
+fi
+if [[ ${WKD} == /home/${CUR} ]]; then
+	echo "Should not be run in home directory, cd to /data/somedir first!"
+	exit -1
+fi
+#
+# Configuration reading # TODO add parametric input
+USR=$(param-read "USR")
+GRP=$(param-read "GRP")
+#
+# Effective run
+perm-check ${USR} ${GRP}
+#exit 0# Exit here if you only want to check # TODO add parametric input
+perm-mod ${USR} ${GRP}
+echo "Finished"
